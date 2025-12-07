fea7e1c60e7037b16be5888a1baf325c4facb31e
lopxans <lopxans@gmail.com>
2025-12-07 13:37:00 +0545

    One to One Relationship

---
+++ P2/app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Profile, Page, Like
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'city']
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'profile', 'page_name']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'likes']
---
+++ P2/app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # yesari yata meaning full comment gara sabai ma tara enligh ma hai bujne gari limited commet
+    # user = models.OneToOneField(User, on_delete=models.PROTECT)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'is_staff': True})
+    # user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+class Page(models.Model):
+    profile = models.OneToOneField(User, on_delete=models.CASCADE)
+    page_name = models.CharField(max_length=100)
+    
+class Like(Page):
+    page = models.OneToOneField(Page, on_delete=models.CASCADE, parent_link=True)
+    likes = models.IntegerField()
