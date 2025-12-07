6ba6242decea3353af8861846c99d218c10cfb32
lopxans <lopxans@gmail.com>
2025-12-07 12:19:45 +0545

    Model

--- P1/app/urls.py
+++ P1/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('', student_list),
+    path('<int:pk>/', student_detail),
 ]
--- P1/app/views.py
+++ P1/app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+# Student list - Get all data 
+def student_list(request):
+    student = Student.objects.all()
+    return render(request, 'app/student.html', {'student': student})
+
+# Student detail - Get by it's i
+def student_detail(request, pk):
+    student = Student.objects.get(pk=pk)
+    return render(request, 'app/student.html', {'student': student})
+
+# Student Create
+def student_detail(request):
+    student = Student.objects.get()
+    return render(request, 'app/student_form.html', {'student': student})
+
+# Student update
+def student_detail(request, pk):
+    student = Student.objects.get(pk=pk)
+    return render(request, 'app/student_form.html', {'student': student})
+
+# Student delete
+def student_detail(request, pk):
+    student = Student.objects.get(pk=pk)
